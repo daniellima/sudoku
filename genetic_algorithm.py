221b83bbe8f3bb60509f40db7bb3c5d5ae8c652b
Annanda Sousa <annanda.sousa@gmail.com>
2016-02-03 15:16:34 -0200

    metodo que gera um individuo

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,20 @@
 # coding: utf-8
 from __future__ import division
+import random
 
-class GeneticAlgotithm:
+from sudoku import Sudoku
 
-    def __init__(self):
+class GeneticAlgorithm:
+
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
         pass
+
+    def generate_index_list(self):
+        individuo = []
+        indexed_list_size = len(self.puzzle.emptyIndexes)
+        for i in range(len(self.puzzle.emptyIndexes)):
+            choice = random.choice(range(indexed_list_size))
+            individuo.append(choice)
+            indexed_list_size -= 1
+        return individuo
---
+++ main_genetic.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from __future__ import division
+
+# Trabalho Sudoku
+# Alunos: Annanda Sousa, Daniel Lima e Tayssa Vandelli
+
+# Ideia do código
+"""
+Heurística escolhida:
+Minimizar a quantidade de erros de números dispostos no tabuleiro.
+
+"""
+
+from genetic_solver import GeneticSolver
+from genetic_algorithm import GeneticAlgorithm
+from sudoku import Sudoku
+
+_ = Sudoku.EMPTY
+sudoku = Sudoku([
+    _, _, _,   7, _, _,   _, _, _,
+    1, _, _,   _, _, _,   _, _, _,
+    _, _, _,   4, 3, _,   2, _, _,
+
+    _, _, _,   _, _, _,   _, _, 6,
+    _, _, _,   5, _, 9,   _, _, _,
+    _, _, _,   _, _, _,   4, 1, 8,
+
+    _, _, _,   _, 8, 1,   _, _, _,
+    _, _, 2,   _, _, _,   _, 5, _,
+    _, 4, _,   _, _, _,   3, _, _
+])
+
+ga = GeneticAlgorithm(sudoku)
+
+individuo = ga.generate_index_list()
+
+print(individuo)
+print(len(individuo))
