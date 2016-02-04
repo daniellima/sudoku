1371c3c7558ce5ed475182d7749517c97963cbbc
Annanda Sousa <annanda.sousa@gmail.com>
2016-02-04 00:13:08 -0200

    colocando legenda no grafico

--- gerar_grafico.py
+++ gerar_grafico.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def gerar_grafico(nome):
+def gerar_grafico(name, label, title):
     fig = plt.figure()
     steps = [50, 100, 200, 500, 1000]
     errors = [68.05, 67.32, 66.21, 63.15, 60.43]
@@ -13,15 +13,32 @@ def gerar_grafico(nome):
     X = steps
     width = .35
     ind = np.arange(len(Y))
-    plt.bar(ind, Y)
+    plt.bar(ind, Y, label=label)
+
     plt.xticks(ind + width / 2, X)
 
-    plt.savefig("%s.png" % nome)
-    plt.close(fig)
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        nome = sys.argv[1]
-    else:
-        nome = 'media_erros_passos'
+    plt.title(title)
+    # Now add the legend with some customizations.
+    legend = plt.legend(loc='best', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
 
-    gerar_grafico(nome)
+    # Set the fontsize
+    for label in legend.get_texts():
+        label.set_fontsize('large')
+
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)  # the legend line width
+
+    plt.savefig("%s.png" % name)
+    plt.close(fig)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        _, name, label, title = sys.argv
+    else:
+        name = 'media_erros_passos'
+        label = 'variavel'
+        title = 'titulo'
+
+    gerar_grafico(name, label, title)
