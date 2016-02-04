45fc6b26f0a072c17ebfe274f1186865e3787f71
Annanda Sousa <annanda.sousa@gmail.com>
2016-02-03 23:22:44 -0200

    Para fazer varios testes

--- main_genetic.py
+++ main_genetic.py
@@ -13,71 +13,64 @@ Minimizar a quantidade de erros de números dispostos no tabuleiro.
 from genetic_solver import GeneticSolver
 from genetic_algorithm import GeneticAlgorithm
 from sudoku import Sudoku
+from utils import get_initial_sudoku
 
-_ = Sudoku.EMPTY
-sudoku = Sudoku([
-    _, _, _,   7, _, _,   _, _, _,
-    1, _, _,   _, _, _,   _, _, _,
-    _, _, _,   4, 3, _,   2, _, _,
+tests = 100
+evaluate_number = 0
+for i in range(tests):
+    sudoku = get_initial_sudoku()
 
-    _, _, _,   _, _, _,   _, _, 6,
-    _, _, _,   5, _, 9,   _, _, _,
-    _, _, _,   _, _, _,   4, 1, 8,
+    ga = GeneticAlgorithm(sudoku)
 
-    _, _, _,   _, 8, 1,   _, _, _,
-    _, _, 2,   _, _, _,   _, 5, _,
-    _, 4, _,   _, _, _,   3, _, _
-])
+    solver = GeneticSolver(
+        sudoku,
+        steps = 10000,
+        population_size = 6,
+        crossover_chance = 0.8,
+        mutation_chance = 0.01
+        )
 
-ga = GeneticAlgorithm(sudoku)
+    population = solver.solve()
 
-solver = GeneticSolver(
-    sudoku,
-    steps = 500,
-    population_size = 6,
-    crossover_chance = 0.8,
-    mutation_chance = 0.01
-    )
+    population.sort(key=lambda ind: ga.fitness_function(ind))
 
-population = solver.solve()
+    ind = ga.get_individual_from_indexed_list(population[-1:][0])
 
-population.sort(key=lambda ind: ga.fitness_function(ind))
+    sudoku.fill_missing(ind)
 
-ind = ga.get_individual_from_indexed_list(population[-1:][0])
+    print(sudoku)
+    print(sudoku.evaluate())
+    evaluate_number = evaluate_number + sudoku.evaluate()
 
-sudoku.fill_missing(ind)
+    um, dois, tres, quatro, cinco, seis, sete, oito, nove = 0,0,0,0,0,0,0,0,0
+    for i in sudoku.puzzle:
+     if i == 1:
+         um += 1
+     elif i == 2:
+         dois += 1
+     elif i == 3:
+         tres += 1
+     elif i == 4:
+         quatro += 1
+     elif i == 5:
+         cinco += 1
+     elif i == 6:
+         seis += 1
+     elif i == 7:
+         sete += 1
+     elif i == 8:
+         oito += 1
+     elif i == 9:
+         nove += 1
 
-print(sudoku)
-print(sudoku.evaluate())
+    print('\nUm', um)
+    print('Dois', dois)
+    print('Tres', tres)
+    print('Quatro', quatro)
+    print('Cinco', cinco)
+    print('Seis', seis)
+    print('Sete', sete)
+    print('Oito', oito)
+    print('Nove', nove)
 
-um, dois, tres, quatro, cinco, seis, sete, oito, nove = 0,0,0,0,0,0,0,0,0
-for i in sudoku.puzzle:
- if i == 1:
-     um += 1
- elif i == 2:
-     dois += 1
- elif i == 3:
-     tres += 1
- elif i == 4:
-     quatro += 1
- elif i == 5:
-     cinco += 1
- elif i == 6:
-     seis += 1
- elif i == 7:
-     sete += 1
- elif i == 8:
-     oito += 1
- elif i == 9:
-     nove += 1
-
-print('\nUm', um)
-print('Dois', dois)
-print('Tres', tres)
-print('Quatro', quatro)
-print('Cinco', cinco)
-print('Seis', seis)
-print('Sete', sete)
-print('Oito', oito)
-print('Nove', nove)
-
+print(evaluate_number/tests)
